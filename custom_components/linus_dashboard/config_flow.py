8565d88c2a12ec0eb8de23a646ae9ae1b4da6bc9
Julien-Decoen <julien.decoen.pro@gmail.com>
2024-11-16 01:21:07 +0000

    fix config flow

--- custom_components/linus_dashboard/config_flow.py
+++ custom_components/linus_dashboard/config_flow.py
@@ -5,11 +5,12 @@ from __future__ import annotations
 import voluptuous as vol
 from homeassistant import config_entries, data_entry_flow
 from homeassistant.helpers import selector
+from homeassistant.core import callback
 
 from .const import DOMAIN
 
 
-class BlueprintFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
+class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Config flow for Linus Dashboard."""
 
     VERSION = 1
@@ -18,31 +19,40 @@ class BlueprintFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         self, user_input: dict | None = None
     ) -> data_entry_flow.FlowResult:
         """Handle a flow initialized by the user."""
-        _errors = {}
 
-        # If user input is provided, create the entry
-        if user_input is not None:
-            return self.async_create_entry(
-                title="Configuration",
-                data=user_input,
-            )
+        if self._async_current_entries():
+            return self.async_abort(reason="single_instance_allowed")
 
-        # Show the form to the user with the required fields
-        return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema(
-                {
-                    vol.Optional("alarm_entity_id"): selector.EntitySelector(
-                        selector.EntitySelectorConfig(
-                            domain="alarm_control_panel",
-                        ),
-                    ),
-                    vol.Optional("weather_entity_id"): selector.EntitySelector(
-                        selector.EntitySelectorConfig(
-                            domain="weather",
-                        ),
-                    ),
-                },
-            ),
-            errors=_errors,
+        return self.async_create_entry(
+            title="",
+            data=user_input,
         )
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        return EditFlow(config_entry)
+
+
+class EditFlow(config_entries.OptionsFlow):
+    def __init__(self, config_entry):
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
+
+        schema = {
+            vol.Optional("alarm_entity_id"): selector.EntitySelector(
+                selector.EntitySelectorConfig(
+                    domain="alarm_control_panel",
+                ),
+            ),
+            vol.Optional("weather_entity_id"): selector.EntitySelector(
+                selector.EntitySelectorConfig(
+                    domain="weather",
+                ),
+            ),
+        }
+
+        return self.async_show_form(step_id="init", data_schema=vol.Schema(schema))

57fe84b0cd7a8b0b2ef2968e08af321b9b517dcc
Julien-Decoen <julien.decoen.pro@gmail.com>
2024-12-02 01:29:07 +0000

    fix lint

--- src/popups/AreaInformationsPopup.ts
+++ src/popups/AreaInformationsPopup.ts
@@ -156,7 +156,7 @@ class AreaInformations extends AbstractPopup {
                             ] : []),
                             {
                                 type: "custom:mushroom-template-card",
-                                primary: `Capteurs utilisé pour la détection de présence :`,
+                                primary: Helper.localize("component.linus_dashboard.entity.button.area_state_popup.name"),
                                 card_mod: {
                                     style: `ha-card {padding: 4px 12px!important;}`
                                 }

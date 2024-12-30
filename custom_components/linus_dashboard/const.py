a29bf8078992d2811a104d1323428b37d4535713
Julien-Decoen <julien.decoen.pro@gmail.com>
2024-12-30 23:35:55 +0000

    bump: bump version to 1.0.6

--- custom_components/linus_dashboard/const.py
+++ custom_components/linus_dashboard/const.py
@@ -6,7 +6,7 @@ LOGGER: Logger = getLogger(__package__)
 
 NAME = "Linus Dahboard"
 DOMAIN = "linus_dashboard"
-VERSION = "1.0.5"
+VERSION = "1.0.6"
 ICON = "mdi:bow-tie"
 
 URL_PANEL = "linus_dashboard_panel"

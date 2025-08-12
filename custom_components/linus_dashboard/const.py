c7b1647e215278c1dd95702f9e0f6475dcd9baa2
Juicy <julien.decoen@gmail.com>
2025-08-12 15:40:13 +0000

    bump version to 1.2.4-alpha.2

--- custom_components/linus_dashboard/const.py
+++ custom_components/linus_dashboard/const.py
@@ -6,7 +6,7 @@ LOGGER: Logger = getLogger(__package__)
 
 NAME = "Linus Dahboard"
 DOMAIN = "linus_dashboard"
-VERSION = "1.2.4-alpha.1"
+VERSION = "1.2.4-alpha.2"
 ICON = "mdi:bow-tie"
 
 URL_PANEL = "linus_dashboard_panel"
--- custom_components/linus_dashboard/www/linus-strategy.js
+++ custom_components/linus_dashboard/www/linus-strategy.js
@@ -7243,7 +7243,7 @@ class LinusStrategy extends HTMLTemplateElement {
     }
 }
 customElements.define("ll-strategy-linus-strategy", LinusStrategy);
-const version = "v1.2.4-alpha.1";
+const version = "v1.2.4-alpha.2";
 console.info("%c Linus Strategy %c ".concat(version, " "), "color: #F5F5DC; background: #004225; font-weight: 700;", "color: #004225; background: #F5F5DC; font-weight: 700;");
 
 

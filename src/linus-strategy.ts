9e61c8a40f9cc85fcd8cb29bd189d698ba5062f6
Julien-Decoen <julien.decoen.pro@gmail.com>
2025-08-12 23:33:54 +0000

    Bump version to 1.2.4-alpha.4

--- src/linus-strategy.ts
+++ src/linus-strategy.ts
@@ -214,7 +214,7 @@ class LinusStrategy extends HTMLTemplateElement {
 
 customElements.define("ll-strategy-linus-strategy", LinusStrategy);
 
-export const version = "v1.2.4-alpha.3";
+export const version = "v1.2.4-alpha.4";
 console.info(
   "%c Linus Strategy %c ".concat(version, " "),
   "color: #F5F5DC; background: #004225; font-weight: 700;", "color: #004225; background: #F5F5DC; font-weight: 700;"

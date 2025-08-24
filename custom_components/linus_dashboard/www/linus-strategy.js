4f77a4f2f0e97fc2d56cc8512206a006c635f450
Juicy <julien.decoen@gmail.com>
2025-08-24 00:05:27 +0000

    chore: update version to 1.2.4

--- custom_components/linus_dashboard/www/linus-strategy.js
+++ custom_components/linus_dashboard/www/linus-strategy.js
@@ -7281,7 +7281,7 @@ class LinusStrategy extends HTMLTemplateElement {
     }
 }
 customElements.define("ll-strategy-linus-strategy", LinusStrategy);
-const version = "v1.2.4-alpha.4";
+const version = "v1.2.4";
 console.info("%c Linus Strategy %c ".concat(version, " "), "color: #F5F5DC; background: #004225; font-weight: 700;", "color: #004225; background: #F5F5DC; font-weight: 700;");
 
 

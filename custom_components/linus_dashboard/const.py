c4973db9710c923a75e0b9675297120404987587
Juicy <julien.decoen@gmail.com>
2025-09-25 10:56:50 +0000

    feat: improve configuration with additionnal exclude options and multiple alarm

--- custom_components/linus_dashboard/const.py
+++ custom_components/linus_dashboard/const.py
@@ -11,11 +11,12 @@ ICON = "mdi:bow-tie"
 
 URL_PANEL = "linus_dashboard_panel"
 
-CONF_ALARM_ENTITY = "alarm_entity"
-CONF_ALARM_ENTITY_ID = "alarm_entity_id"
+CONF_ALARM_ENTITIES = "alarm_entities"  # Liste d'alarmes
+CONF_ALARM_ENTITY_IDS = "alarm_entity_ids"  # Liste d'IDs d'alarmes
 CONF_WEATHER_ENTITY = "weather_entity"
 CONF_WEATHER_ENTITY_ID = "weather_entity_id"
 CONF_EXCLUDED_DOMAINS = "excluded_domains"
 CONF_EXCLUDED_DEVICE_CLASSES = "excluded_device_classes"
-CONF_EXCLUDED_ENTITIES = "excluded_entities"
+CONF_EXCLUDED_TARGETS = "excluded_targets"
 CONF_HIDE_GREETING = "hide_greeting"
+CONF_EXCLUDED_INTEGRATIONS = "excluded_integrations"  # Intégrations à exclure

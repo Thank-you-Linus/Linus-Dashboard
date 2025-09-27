5f634c4a3577e4bc58ae8cba6e32b70c5ec48de4
Juicy <julien.decoen@gmail.com>
2025-09-27 21:21:41 +0000

    Clean const

--- custom_components/linus_dashboard/const.py
+++ custom_components/linus_dashboard/const.py
@@ -11,12 +11,11 @@ ICON = "mdi:bow-tie"
 
 URL_PANEL = "linus_dashboard_panel"
 
-CONF_ALARM_ENTITIES = "alarm_entities"  # Liste d'alarmes
-CONF_ALARM_ENTITY_IDS = "alarm_entity_ids"  # Liste d'IDs d'alarmes
+CONF_ALARM_ENTITY_IDS = "alarm_entity_ids"
 CONF_WEATHER_ENTITY = "weather_entity"
 CONF_WEATHER_ENTITY_ID = "weather_entity_id"
 CONF_EXCLUDED_DOMAINS = "excluded_domains"
 CONF_EXCLUDED_DEVICE_CLASSES = "excluded_device_classes"
 CONF_EXCLUDED_TARGETS = "excluded_targets"
 CONF_HIDE_GREETING = "hide_greeting"
-CONF_EXCLUDED_INTEGRATIONS = "excluded_integrations"  # Intégrations à exclure
+CONF_EXCLUDED_INTEGRATIONS = "excluded_integrations"
